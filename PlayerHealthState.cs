8b027fd24c289dbb87633d00da6d1fb46462b030
z00diq <45630610+z00diq@users.noreply.github.com>
2021-11-09 20:03:09 +0300

    Add files via upload

---
+++ PlayerHealthState.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class PlayerHealthState: MonoBehaviour
+    {
+        [SerializeField] PlayerModel _player;
+        [SerializeField] float  _damage;
+        [SerializeField] float _heal;
+        
+        public void GetDamage()
+        {
+            _player.GetDamage(_damage);
+        }
+
+        public void GetHeal()
+        {
+            _player.GetHeal(_heal);
+        }
+    }
+}
---
+++ PlayerModel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class PlayerModel: MonoBehaviour
+    {
+        [SerializeField] private float _health;
+
+        public float Health => _health;
+
+        public void GetDamage(float damage)
+        {
+            _health -= damage;
+        }
+
+        public void GetHeal(float heal)
+        {
+            _health += heal;
+        }
+    }
+}
