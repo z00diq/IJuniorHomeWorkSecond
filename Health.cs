32d483d6b0a811cc98258c906b216de24e18f98e
z00diq <45630610+z00diq@users.noreply.github.com>
2021-11-14 15:16:59 +0300

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Health: MonoBehaviour
+    {
+        [SerializeField] private float _health;
+        [SerializeField] private float _healDamageValue;
+
+        public delegate void HealthChanger();
+
+        public event HealthChanger HealthIsChanged;
+
+        public float CurrentHealth => _health;
+
+        public void GetDamage()
+        {
+            _health -= _healDamageValue;
+            HealthIsChanged?.Invoke();
+        }
+
+        public void GetHeal()
+        {
+            _health += _healDamageValue;
+            HealthIsChanged?.Invoke();
+        }
+    }
+}
--- HealthBar.cs
+++ HealthBar.cs
@@ -7,27 +7,30 @@ namespace Assets.Scripts
     [RequireComponent(typeof(Slider))]
     class HealthBar : MonoBehaviour
     {
-        [SerializeField] private PlayerHealth _player;
+        [SerializeField] private Health _player;
         [SerializeField] private float _duratuion;
 
         private Slider _slider;
         private float _currentValue;
         private float _normalizedValue;
 
-        public PlayerHealth Player => _player;
-
         private void Awake()
         {
             _slider = GetComponent<Slider>();
             _slider.value = _slider.maxValue;
-            _normalizedValue = _slider.maxValue / Player.Health;
-            _player.RenderHealth += RenderFill;
+            _normalizedValue =  _slider.maxValue / _player.CurrentHealth;
+            _player.HealthIsChanged += RenderFill;
         }
 
+        private void OnDisable()
+        {
+            _player.HealthIsChanged -= RenderFill;
+        }
         private void RenderFill()    
         {
-            _currentValue = Player.Health*_normalizedValue;
+            _currentValue = _player.CurrentHealth * _normalizedValue;
             _slider.DOValue(_currentValue, _duratuion);
         }
+
     }
 }
